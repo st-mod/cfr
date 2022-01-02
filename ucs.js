865a6a0e870fe77b0d172ef34d74bc51a7ec5343
DDU <ddu6@protonmail.com>
2022-01-03 04:35:11 +0800

    update

--- ucs.js
+++ ucs.js
@@ -1,37 +1,37 @@
-export const a=async (unit,compiler)=>{
-    const element=document.createElement('a')
-    let {href,src}=unit.options
-    if(href===true){
-        href=''
-    }else if(typeof href==='number'){
-        href=href.toString()
+export const a = async (unit, compiler) => {
+    const element = document.createElement('a')
+    let {href, src} = unit.options
+    if (href === true) {
+        href = ''
+    } else if (typeof href === 'number') {
+        href = href.toString()
     }
-    if(src===true){
-        src=''
-    }else if(typeof src==='number'){
-        src=src.toString()
+    if (src === true) {
+        src = ''
+    } else if (typeof src === 'number') {
+        src = src.toString()
     }
-    if(typeof href==='string'){
-        element.textContent=href
-        if(!href.startsWith('#')){
-            element.target='_blank'
-            const url=new URL(href,compiler.context.dir)
-            if(url.origin.endsWith('.vscode-resource.vscode-webview.net')){
-                element.href=`command:vscode.open?${encodeURIComponent(JSON.stringify([{scheme:'file',path:url.pathname},-2]))}`
+    if (typeof href === 'string') {
+        element.textContent = href
+        if (!href.startsWith('#')) {
+            element.target = '_blank'
+            const url = new URL(href, compiler.context.dir)
+            if (url.origin.endsWith('.vscode-resource.vscode-webview.net')) {
+                element.href = `command:vscode.open?${encodeURIComponent(JSON.stringify([{scheme: 'file', path: url.pathname}, -2]))}`
             }
         }
-    }else if(typeof src==='string'){
-        element.target='_blank'
-        element.textContent=src
-        const url=new URL(src,compiler.context.dir)
-        if(url.origin.endsWith('.vscode-resource.vscode-webview.net')){
-            element.href=`command:st-lang.preview-path?${encodeURIComponent(JSON.stringify([url.pathname,undefined,undefined,decodeURIComponent(url.hash.slice(1))]))}`
-        }else{
-            element.href=`?src=${encodeURIComponent(url.href)}${url.hash}`
+    } else if (typeof src === 'string') {
+        element.target = '_blank'
+        element.textContent = src
+        const url = new URL(src, compiler.context.dir)
+        if (url.origin.endsWith('.vscode-resource.vscode-webview.net')) {
+            element.href = `command:st-lang.preview-path?${encodeURIComponent(JSON.stringify([url.pathname, undefined, undefined, decodeURIComponent(url.hash.slice(1))]))}`
+        } else {
+            element.href = `?src=${encodeURIComponent(url.href)}${url.hash}`
         }
     }
-    if(unit.children.length>0){
-        element.innerHTML=''
+    if (unit.children.length > 0) {
+        element.innerHTML = ''
         element.append(await compiler.compileInlineSTDN(unit.children))
     }
     return element
