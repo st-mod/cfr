5924ce736baa1c2d64879d2afededd4843924d02
DDU <ddu6@protonmail.com>
2022-01-26 05:30:12 +0800

    update

--- ucs.js
+++ ucs.js
@@ -15,7 +15,7 @@ export const a = async (unit, compiler) => {
         element.textContent = href
         if (!href.startsWith('#')) {
             element.target = '_blank'
-            const url = new URL(href, compiler.context.dir)
+            const url = new URL(compiler.context.urlToAbsURL(href, unit))
             if (url.origin.endsWith('.vscode-resource.vscode-webview.net')) {
                 element.href = `command:vscode.open?${encodeURIComponent(JSON.stringify([{scheme: 'file', path: url.pathname}, -2]))}`
             }
@@ -23,7 +23,7 @@ export const a = async (unit, compiler) => {
     } else if (typeof src === 'string') {
         element.target = '_blank'
         element.textContent = src
-        const url = new URL(src, compiler.context.dir)
+        const url = new URL(compiler.context.urlToAbsURL(src, unit))
         if (url.origin.endsWith('.vscode-resource.vscode-webview.net')) {
             element.href = `command:st-lang.preview-path?${encodeURIComponent(JSON.stringify([url.pathname, undefined, undefined, decodeURIComponent(url.hash.slice(1))]))}`
         } else {
